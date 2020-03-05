8eb8b183ec83c68f8ee1f59181b3dec5c0364f6a
Dominik Suchner <s19036@pjwstk.edu.pl>
2020-03-05 13:10:32 +0100

    New commit

--- APBD_05.03/APBD_05.03/Program.cs
+++ APBD_05.03/APBD_05.03/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace APBD_05._03
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            for (int i = 0; i < 20; i++)
-            {
 
+            HttpClient client = new HttpClient();
+            var result = await client.GetAsync("http://www.wp.pl"); //Task<HttpResponseMassage>
+
+            if (!result.IsSuccessStatusCode) return;
+
+            string html = await result.Content.ReadAsStringAsync();
+            var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+", RegexOptions.IgnoreCase);
+            var matcher = regex.Matches(html);
+            foreach(var m in matcher)
+            {
+                Console.WriteLine(m);
             }
         }
     }
