08bac209bb796977b03ad8e26ecc88133ead47f5
Dominik Suchner <s19036@pjwstk.edu.pl>
2020-03-05 12:41:22 +0100

    First commit from visual

---
+++ APBD_05.03/APBD_05.03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace APBD_05._03
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 20; i++)
+            {
+
+            }
+        }
+    }
+}
